dad0408b181a82a8a5a05e3d80aa95af65492cfd
Nikolay Belousov <belousovnia@gmail.com>
2022-08-15 16:03:38 +0400

    modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/FirstSection.js 	new file:   src/components/Header.js 	new file:   src/css/constant.scss 	new file:   src/css/first-section.scss 	new file:   src/css/header.scss 	new file:   src/css/main.css 	new file:   src/css/main.css.map 	modified:   src/css/main.scss 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	new file:   src/media/Lunch_atop_a_Skyscraper 1.jpg 	deleted:    src/setupTests.js 	modified:   yarn.lock

---
+++ src/components/FirstSection.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function FirstSection() {
+  return (
+    <section className='first-section'>
+      <div className='first-section__filter'>
+        <div className='first-section__content-container'>
+          <h1 className='first-section__title'>
+            Говорят, никогда не поздно сменить профессию
+          </h1>
+          <p className='first-section__p'>
+            Сделай круто тестовое задание и у тебя получится
+          </p>
+          <button className='main-button'>
+            Проще простого!
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
